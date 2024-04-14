07a9ae13ec6ce8a89aaaca1633bb7a3200bec995
Bukita89 <maria@secretstache.com>
2024-04-14 14:17:30 +0300

    add argument for having composer

--- src/Console/SetupTemplate.php
+++ src/Console/SetupTemplate.php
@@ -12,7 +12,7 @@ class SetupTemplate extends Command
      *
      * @var string
      */
-    protected $signature = 'setup:template {template_name}';
+    protected $signature = 'setup:template {template_name} {has_composer=false}';
 
     /**
      * The console command description.
@@ -29,20 +29,19 @@ class SetupTemplate extends Command
     public function handle()
     {
         $template_name = $this->argument('template_name');
-
-        $this->info($template_name);
+        $has_composer = $this->argument('has_composer');
 
         $files = new Filesystem();
 
         $acf_source = dirname(__DIR__) . '/templates/' . $template_name . '/acf';
         $view_source = dirname(__DIR__) . '/templates/' . $template_name . '/view';
 
-        $acf_desctination = app_path('Fields/Templates');
+        $acf_destination = app_path('Fields/Templates');
         $view_destination = resource_path('/views/templates');
 
         $files->copyDirectory(
             $acf_source, 
-            $acf_desctination
+            $acf_destination
         );
 
         $files->copyDirectory(
@@ -50,7 +49,48 @@ class SetupTemplate extends Command
             $view_destination
         );
 
-       $this->info($acf_source);
+        if( $has_composer ){
+            $composer_source = dirname(__DIR__) . '/templates/' . $template_name . '/composer';
+            $composer_destination = app_path('View/Composers/Templates');
+            $composer_name = str_replace(' ', '', ucwords(str_replace('-', ' ', $template_name)));
+
+            $files->copyDirectory(
+                $composer_source, 
+                $composer_destination
+            );
+    
+            //insert template line to composer switch
+            $switch_file_path = app_path('View/Composers/Switches/Templates.php');
+            $switch_file_contents = file_get_contents( $switch_file_path );
+    
+            if( !$switch_file_contents ){
+                $this->info('The composer switch templates file was not found.');
+                return;
+            }
+            
+            $use_pos = strpos( $switch_file_contents, 'use App\View\Composers\SSM;' );
+            if ( $use_pos === false ) {
+                $this->info('The SSM class definition was not found in the switch file.');
+                return;
+            }
+
+            $switch_file_contents = substr_replace( $switch_file_contents, PHP_EOL . 'use App\View\Composers\Templates\\' . $composer_name . ';', $use_pos + strlen( 'use App\View\Composers\SSM;' ), 0 );
+    
+            $switch_pos = strpos( $switch_file_contents, 'switch ($template[\'acf_fc_layout\']) {' );
+            if ( $switch_pos === false ) {
+                $this->info('The switch acf_fc_layout was not found in the switch file.');
+                return;
+            }
+            $switch_file_contents = substr_replace( 
+                $switch_file_contents, 
+                PHP_EOL . ' case (\'' . $template_name . '\'):
+                    $templateData = '. $composer_name . '::getTemplateData($template);
+                    break;', 
+                $switch_pos + strlen( 'switch ($template[\'acf_fc_layout\']) {' ), 0 );
+    
+            // Write back to the file.
+            file_put_contents( $switch_file_path, $switch_file_contents );
+        }
 
        return $this->info( $template_name . ' setup - success');
 
